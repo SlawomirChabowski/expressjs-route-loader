a4a2f2ba3535a78f4261d8b197c94bfdbf5dcd03
Sławek Chabowski <slawomir.chabowski@gmail.com>
2020-03-20 11:28:21 +0100

    Cleared repository

--- src/default.js
+++
@@ -1,3 +0,0 @@
-exports.index = (req, res) => {
-  res.send('<h1>Success!</h1><p>Your ExpressJS application runs properly</p>');
-};
--- src/errorPages.js
+++
@@ -1,9 +0,0 @@
-exports._404 = (req, res) => {
-  res.status(404);
-  res.send(`
-    <h1>Error 404</h1>
-    <p>Could not find the following route:
-      <em>${req.originalUrl}</em>
-    </p>
-  `);
-};
